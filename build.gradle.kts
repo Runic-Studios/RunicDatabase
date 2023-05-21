07708ca21e2d9e0f49f53bda65289d75ef9dabb8
Excel619 <excel8392@gmail.com>
2023-05-20 23:48:30 -0600

    Updated buildscript and plugin.yml dependencies, added config.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 group = "com.runicrealms.plugin"
 version = "1.0-SNAPSHOT"
+val artifactName = "database"
 
 dependencies {
     compileOnly(commonLibs.taskchain)
@@ -26,12 +27,9 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = "com.runicrealms.plugin"
-            artifactId = "database"
+            artifactId = artifactName
             version = "1.0-SNAPSHOT"
             from(components["java"])
         }
     }
 }
-
-tasks.register("wrapper")
-tasks.register("prepareKotlinBuildScriptModel")

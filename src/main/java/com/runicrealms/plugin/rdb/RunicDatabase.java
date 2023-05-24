62a205ab9353db2d99e65cdf96f3ae7e66a74c1b
Skyfallin <dimetrius.hightower@gmail.com>
2023-05-24 09:02:11 -0400

    fix for database issues, testing push to github

--- src/main/java/com/runicrealms/plugin/rdb/RunicDatabase.java
+++ src/main/java/com/runicrealms/plugin/rdb/RunicDatabase.java
@@ -7,12 +7,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Level;
 
 public class RunicDatabase extends JavaPlugin {
-
     private static RunicDatabase instance;
 
     private String databaseName;
     private String connectionString;
-
     private RunicDatabaseAPI databaseAPI;
 
     public static RunicDatabaseAPI getAPI() {
@@ -42,13 +40,14 @@ public class RunicDatabase extends JavaPlugin {
         databaseName = this.getConfig().getString("database");
         if (databaseName == null) throw new IllegalStateException("Missing database name! Aborting startup.");
         connectionString = this.getConfig().getString("connection-string");
+        Bukkit.getLogger().severe("string is " + connectionString);
         if (connectionString == null) throw new IllegalStateException("Missing connection string! Aborting startup.");
 
         Bukkit.getLogger().log(Level.INFO, "[RunicDatabase] Loaded RunicDatabase");
     }
 
     public void setAPIImplementation(RunicDatabaseAPI databaseAPI) {
-        if (this.databaseAPI != null) throw new IllegalStateException("Already initialized RuincDatabaseAPI!");
+        if (this.databaseAPI != null) throw new IllegalStateException("Already initialized RunicDatabaseAPI!");
         this.databaseAPI = databaseAPI;
         Bukkit.getPluginManager().callEvent(new DatabaseInitializeEvent());
     }

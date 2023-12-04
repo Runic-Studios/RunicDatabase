232551495d4b9c7e5612c7b9333d0e75ab0cdbe6
Excel619 <excel8392@gmail.com>
2023-12-04 12:59:08 -0500

    Updated build.gradle.kts to improved superproject version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,3 @@
-val artifactName = "database"
-val rrGroup: String by rootProject.extra
-val rrVersion: String by rootProject.extra
-
-plugins {
-    `java-library`
-    `maven-publish`
-}
-
-group = rrGroup
-version = rrVersion
-
 dependencies {
     compileOnly(commonLibs.taskchain)
     compileOnly(commonLibs.springdatamongodb)
@@ -22,19 +10,4 @@ dependencies {
     compileOnly(project(":Projects:Common"))
     compileOnly(commonLibs.log4jcore)
     compileOnly(commonLibs.log4japi)
-}
-
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = rrGroup
-            artifactId = artifactName
-            version = rrVersion
-            from(components["java"])
-        }
-    }
 }
